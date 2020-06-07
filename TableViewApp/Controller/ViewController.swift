de5cb89a85e48df769a2267a6868c90e1da92efe
k_kita <ak2205d@icloud.com>
2020-06-07 16:04:30 +0900

    firstcommit

---
+++ TableViewApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by kita kensuke on 2020/06/07.
+//  Copyright © 2020 kita kensuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
